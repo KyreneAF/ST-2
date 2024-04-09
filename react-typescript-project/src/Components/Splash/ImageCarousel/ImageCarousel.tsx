8b7999594fb3acc96e99d6ff0c20436f032f71ca
Kyrene <Kyrene.Aflores@gmail.com>
2024-04-09 15:47:49 -0500

    finished styling on splash page

--- react-typescript-project/src/Components/Splash/ImageCarousel/ImageCarousel.tsx
+++ react-typescript-project/src/Components/Splash/ImageCarousel/ImageCarousel.tsx
@@ -58,22 +58,22 @@ const ImageCarousel = () => {
     <div className="image-carousel">
       <Slider {...settings}>
         <div>
-          <img className="carousel-img" src={image1} alt="Image 1" />
+        <img className="carousel-img" src={image1} alt="Image 1" />
+        </div>
+        {/* <div>
+          <img className="carousel-img" src={image2} alt="Image 2" />
+        </div> */}
+        <div>
+          <img className="carousel-img" src={image3} alt="Image 3" />
         </div>
         <div>
-          <img src={image2} alt="Image 2" />
+        <img className="carousel-img" src={image4} alt="Image 4" />
         </div>
         <div>
-          <img src={image3} alt="Image 3" />
+          <img className="carousel-img" src={image5} alt="Image 5" />
         </div>
         <div>
-          <img src={image4} alt="Image 4" />
-        </div>
-        <div>
-          <img src={image5} alt="Image 5" />
-        </div>
-        <div>
-          <img src={image6} alt="Image 6" />
+          <img className="carousel-img" src={image6} alt="Image 6" />
         </div>
         {/* Add more image slides as needed */}
       </Slider>

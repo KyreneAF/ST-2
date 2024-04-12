c0cea82c9bd3b4c22461a14f2496f484748bfc76
Kyrene <Kyrene.Aflores@gmail.com>
2024-04-12 14:13:00 -0600

    finished menu page styling

--- react-typescript-project/src/Components/Menu/Menu.tsx
+++ react-typescript-project/src/Components/Menu/Menu.tsx
@@ -1,15 +1,22 @@
 import regularMenu from "../../assets/images/regularMenu.png"; // These are imports to bring images in assets folder
 import spikedMenu from "../../assets/images/spikedMenu.jpeg";
+import "../../../sass/stylesheets/components/_menu.scss";
 
 const Menu = () => {
   //This comopenent renders the menu image on its own page
   return (
     <div className="menu">
+      <div className="__menu-header">
+        <h1>Menus</h1>
+      </div>
       <div className="__regular-menu-img">
-        <img src={regularMenu} />
+        <img className="__menu-img" src={regularMenu} />
+      </div>
+      <div className="__menu-header">
+        <h1>Spiked Menu</h1>
       </div>
       <div className="__spiked-menu-img">
-        <img src={spikedMenu} />
+        <img className="__menu-img" src={spikedMenu} />
       </div>
     </div>
   );

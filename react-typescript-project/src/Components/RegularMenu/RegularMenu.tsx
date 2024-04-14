9e44be22f0b5a0ff748efd2c73ed3ddebc903043
Kyrene <Kyrene.Aflores@gmail.com>
2024-04-14 14:38:49 -0600

    fixed flex directio

--- react-typescript-project/src/Components/RegularMenu/RegularMenu.tsx
+++ react-typescript-project/src/Components/RegularMenu/RegularMenu.tsx
@@ -4,21 +4,22 @@ const RegularMenu = () => {
   console.log(drinks);
   return (
     <div className="regular-menu">
-      <div className="all-drinks"></div>
-      {drinks.map((drinkObj, index) => (
-        <div key={index} className="single-drink">
-          <span>{drinkObj.name}</span>
-          {drinkObj.ingredients.map((ingredient, index) => (
+      <div className="all-drinks">
+        {drinks.map((drinkObj, index) => (
+          <div key={index} className="single-drink">
+            <span>{drinkObj.name}</span>
             <div key={index} className="ingredients">
-              <span>{ingredient}</span>
+              {drinkObj.ingredients.map((ingredient, index) => (
+                <span>{ingredient}</span>
+              ))}
             </div>
-          ))}
-          <div className="price">
-            <span>{drinkObj.size[0]}</span>
-            <span>{drinkObj.size[1]}</span>
+            <div className="price">
+              <span>{drinkObj.size[0]}</span>
+              <span>{drinkObj.size[1]}</span>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };

f1bd8d7e53e0ec7bf90d9a41fbf8b83945586467
Kyrene <Kyrene.Aflores@gmail.com>
2024-04-17 15:48:10 -0600

    pushing changes to main and config

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { createRoot } from "react-dom";
 // import App from "./App.tsx";
 import "./index.css";
 import { router } from "./Routes/Routes.tsx";
 import { RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     {/* <App router={router} /> */}
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+import { BrowserRouter } from "react-router-dom";
+// const rootElement = document.getElementById("root");
+// if (!rootElement) throw new Error("failed to find the root");
+
+// const root = ReactDOM.createRoot(rootElement);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App router={router} /> */}
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+// // Render the component into the root container
+// ReactDOM.render(<App />, rootElement);
